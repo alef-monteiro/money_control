fb009c1994512db16bffbbb42c4fc02fef1c771e
álef monteiro <72264836+alef-monteiro@users.noreply.github.com>
2025-01-16 16:34:24 -0300

    fix: Adjust main page and api urls (#1)

--- money_control/urls.py
+++ money_control/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('finance.urls')),
-    path('', include('finance.urls')),
+    path('api/', include('finance.urls')),  # Inclui todas as rotas da API no prefixo /api/
 ]
